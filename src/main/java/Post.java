5a3d79c6c3f8746933eba1feecb74d620e512b12
Andrew S <samum_94@mail.ru>
2023-03-06 08:34:12 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public Boolean subscription;
+    public FormDate birthday;
+}
